a4652d91f3b7c4047343d4c477a158f209dbda4a
OnlyR0ck <guitarilu2019@gmail.com>
2021-02-10 00:53:30 +0300

    Fix torus phys and added able to track the hit

---
+++ Assets/Scripts/Main/GoalController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalController : MonoBehaviour
+{
+    #region Fields
+
+    private BoxCollider _scoreZone;
+     public Transform _playerTransform;
+    [SerializeField] private Vector3 _enemyPosition;
+    private bool _playerAlreadyHit = false;
+    private bool _enemyAlreadyHit = false;
+    
+    
+    public static event Action<int> getHit;
+
+
+    #endregion
+    void Start()
+    {
+        
+        _scoreZone = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_scoreZone.bounds.Contains(_playerTransform.position) && !_playerAlreadyHit)
+        {
+            Debug.Log("Player Hit");
+            _playerAlreadyHit = true;
+        }
+        else _playerAlreadyHit = false;
+    }
+}
---
+++ Assets/Scripts/Main/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    #region Fields
+
+    [SerializeField] private Vector3 _mousePosition;
+    private Rigidbody _playerRigidbody;
+    [Range(1, 20)][SerializeField] private float _jumpPower;
+    private float _middleOfTheScreen;
+    private short _directionUnit;
+    
+
+    #endregion
+    
+    void Start()
+    {
+        _playerRigidbody = GetComponent<Rigidbody>();
+        _middleOfTheScreen = Screen.width / 2;
+    }
+
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            _mousePosition = Input.mousePosition;
+            if (_mousePosition.x > _middleOfTheScreen)
+            {
+                _directionUnit = 1;
+            }
+            else
+            {
+                _directionUnit = -1;
+            }
+
+            _playerRigidbody.velocity = Vector3.zero;
+            _playerRigidbody.AddForce(Vector3.up * _jumpPower, ForceMode.VelocityChange);
+            _playerRigidbody.AddForce(Vector3.right * (_jumpPower / 2 * _directionUnit), ForceMode.VelocityChange);
+        }
+
+    }
+}
