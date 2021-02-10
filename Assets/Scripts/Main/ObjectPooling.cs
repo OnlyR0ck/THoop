ed3472854ba7e3291ea199ddbfcd1879725ab6a7
OnlyR0ck <guitarilu2019@gmail.com>
2021-02-10 21:44:01 +0300

    Fixed some bugs

---
+++ Assets/Scripts/Main/ObjectPooling.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooling : MonoBehaviour
+{
+    #region Fields
+
+    public List<GameObject> prefabsToSpawn;
+    public GameObject objectToPool;
+    public int amountPool;
+
+    #endregion
+
+    #region PoolInitialization
+
+    void Start()
+    {
+        prefabsToSpawn = new List<GameObject>();
+        for (int i = 0; i < amountPool; i++)
+        {
+            GameObject temp = (GameObject) Instantiate(objectToPool, this.transform, true);
+            prefabsToSpawn.Add(temp);
+            temp.SetActive(false);
+        }
+    }
+
+    #endregion
+
+    #region GetObjectMethod
+
+    public GameObject GetOre()
+    {
+        for (int i = 0; i < amountPool; i++)
+        {
+            if (!prefabsToSpawn[i].gameObject.activeInHierarchy)
+            {
+                return prefabsToSpawn[i];
+            }
+        }
+        return null;
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/Main/SceneController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+    #region Fields
+
+    public GameObject playerPrefab;
+    public GameObject enemyPrefab;
+    public GameObject spherePrefab;
+    [SerializeField] private readonly Vector3 _playerSpawnPosition = new Vector3(-1.5f, -2.8f);
+    [SerializeField] private readonly Vector3 _enemySpawnPosition = new Vector3(1.5f, -2.8f);
+
+    private ObjectPooling _pool;
+
+    #endregion
+
+    #region OnEnable
+
+    private void OnEnable()
+    {
+        GoalController.getHit += SpawnSphere;
+    }
+
+    #endregion
+
+    #region OnDisable
+
+    private void OnDisable()
+    {
+        GoalController.getHit -= SpawnSphere;
+    }
+
+    #endregion
+
+    #region Initialization
+
+    void Start()
+    {
+        _pool = GetComponent<ObjectPooling>();
+
+    }
+
+    #endregion
+
+    void SpawnSphere(bool winner)
+    {
+        
+    }
+}
